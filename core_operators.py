c34fd55e38d397bba5388639218115de9d48db6b
Scott McKuen <isotropy@gmail.com>
2023-02-19 14:44:12 -0600

    removing unused import

--- core_operators.py
+++ core_operators.py
@@ -1,7 +1,7 @@
 import math
 
 from .__support__ import eipn
-from .states import MatrixOperator, identity_op
+from .states import MatrixOperator
 
 pauli_x = MatrixOperator([[0, 1],
                           [1, 0]])
