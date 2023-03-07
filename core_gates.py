521c2791f7dd317efb757228d2b566d5c527f821
Scott McKuen <isotropy@gmail.com>
2023-03-07 11:39:58 -0600

    adding tensor product of gates; adding option to name a gate; adding standard names for X,Y,Z,H, etc.

--- core_gates.py
+++ core_gates.py
@@ -2,11 +2,11 @@ from .states import identity_op
 from .gates import UnitaryGate
 from .core_operators import *
 
-Identity = UnitaryGate(identity_op)
-PauliX = UnitaryGate(pauli_x)
-PauliY = UnitaryGate(pauli_y)
-PauliZ = UnitaryGate(pauli_z)
-Hadamard = UnitaryGate(hadamard_op)
-S = UnitaryGate(phase_op)
-T = UnitaryGate(pi_over_8_gate_op)
-CNOT = UnitaryGate(cnot_op)
+Identity = UnitaryGate(identity_op, 'I')
+PauliX = UnitaryGate(pauli_x, 'X')
+PauliY = UnitaryGate(pauli_y, 'Y')
+PauliZ = UnitaryGate(pauli_z, 'Z')
+Hadamard = UnitaryGate(hadamard_op, 'H')
+S = UnitaryGate(phase_op, 'S')
+T = UnitaryGate(pi_over_8_gate_op, 'T')
+CNOT = UnitaryGate(cnot_op, 'CNOT')
--- gates.py
+++ gates.py
@@ -1,9 +1,13 @@
+from typing import TypeVar
+
 from .states import MatrixArray, MatrixOperator
 from .qubits import Qubit, QubitSet
 
+TmpUG = TypeVar("TmpUG", bound="UnitaryGate")
+
 
 class UnitaryGate:
-    def __init__(self, matrix: MatrixArray | MatrixOperator):
+    def __init__(self, matrix: MatrixArray | MatrixOperator, name=None):
 
         if isinstance(matrix, MatrixOperator):
             self._matrix = matrix
@@ -12,6 +16,11 @@ class UnitaryGate:
 
         if not self._matrix.is_unitary():
             raise ValueError(f"Input operator {matrix} is not unitary.")
+
+        self._name = name
+
+    def __repr__(self):
+        return f'{self._name}'
 
     def __call__(self, qubit: Qubit, *qubits: Qubit):
         """
@@ -31,8 +40,17 @@ class UnitaryGate:
 
     def __pow__(self, exponent):
         new_matrix = self._matrix.__pow__(exponent)
-        return UnitaryGate(new_matrix)
+        return UnitaryGate(new_matrix, f'{self}^{exponent}')
 
+    def __neg__(self):
+        new_matrix = -self._matrix
+        return UnitaryGate(new_matrix, f'-{self}')
 
     def to_operator(self):
         return self._matrix
+
+    def tensor(self, other: TmpUG) -> TmpUG:
+        op1 = self.to_operator()
+        op2 = other.to_operator()
+        new_op = op1.tensor(op2)
+        return UnitaryGate(matrix=new_op, name=f'{self}x{other}')
